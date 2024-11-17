bf3f0e082761a72dc6309ccb6c34bdae656b8fbd
GazdagB <gazdagbal@gmail.com>
2024-11-17 22:36:51 +0100

    Added primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: 'rgb(var(--color-primary))'
+      }
+    },
   },
   plugins: [],
 }
