70a115a45692932a7e2b2f5e41c2f29009829c3e
GazdagB <gazdagbal@gmail.com>
2024-11-19 22:13:41 +0100

    Added TODO comments

--- src/features/posts/PostsSlice.js
+++ src/features/posts/PostsSlice.js
@@ -1,5 +1,15 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+//TODO: Add Posts to store and comments as well 
+/*
+- Add store 
+- Comments Opened State 
+- Comments Closed 
+- Upvotes Property
+*/
+
+//TODO: Add Reducer: UpVote and DownVote 
+
 const initialState = []; 
 
 const postsSlice = createSlice({
--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-
+//TODO: Create all of the slices and reducers combine them and initiate a store. 
 
 
 const store = configureStore({
