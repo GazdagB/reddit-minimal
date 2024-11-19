927247a02003ad483052fe68f1063d946924f837
Gazdag Balázs <gazdagbal@gmail.com>
2024-11-19 20:37:08 +0100

    Responsivity added to Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { IoSearch } from "react-icons/io5";
 
 const Navbar = () => {
   return (
-    <div className='Navbar w-full shadow-md grid grid-cols-3 px-10 py-5  mb-10'>
+    <div className='Navbar w-full shadow-md grid grid-cols-2 lg:grid-cols-3 px-10 lg:px-20 py-5 mb-10'>
         <div className="flex items-center gap-3 min-w-[230px]">
             <img className="h-8" src={redditIcon} alt="" />
             <p className="text-xl font-bold"><span className="text-blue-600">Reddit</span> Alternate</p>
